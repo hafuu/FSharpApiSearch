1b86fb0435a32fae33fc46dbbc2aa81a19b8f3f9
hafuu <hafuu46@gmail.com>
2016-01-21 21:27:20 +0900

    Initialize

---
+++ src/FSharpApiSearch/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("FSharpApiSearch")>]
+[<assembly: AssemblyProductAttribute("FSharpApiSearch")>]
+[<assembly: AssemblyDescriptionAttribute("F# API search engine")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
