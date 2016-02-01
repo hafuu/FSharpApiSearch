b5c5b4afcbd60f173c67500f89bc7ab04dae6fe3
hafuu <hafuu46@gmail.com>
2016-02-01 19:17:08 +0900

    Setting by the command line arguments.

--- src/FSharpApiSearch.Console/Program.fs
+++ src/FSharpApiSearch.Console/Program.fs
@@ -2,9 +2,41 @@
 open System.Diagnostics
 open System
 
-let searchAndShowResult (client: FSharpApiSearchClient) (query: string) =
-  let sw = Stopwatch.StartNew()
-  let results = client.Search(query)
+type Args = {
+  Query: string option
+  SearchOptions: SearchOptions
+  Help: bool
+}
+
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+module Args =
+  let empty = { Query = None; SearchOptions = SearchOptions.defaultOptions; Help = false }
+
+  let (|Status|_|) (name: string) (str: string) =
+    if str.StartsWith(name) then
+      match str.Substring(name.Length) with
+      | "+" -> Some true
+      | "-" -> Some false
+      | _ -> None
+    else
+      None
+
+  let boolToOptionStatus = function true -> Enabled | false -> Disabled
+
+  let rec parse opt = function
+    | Status "--strict" v :: rest -> parse { opt with SearchOptions = { opt.SearchOptions with StrictQueryVariable = boolToOptionStatus v } } rest
+    | Status "--similarity" v :: rest -> parse { opt with SearchOptions = { opt.SearchOptions with SimilaritySearching = boolToOptionStatus v } } rest
+    | "--help" :: rest -> parse { opt with Help = true } rest
+    | query :: rest ->
+      let q =
+        match opt.Query with
+        | None -> Some query
+        | Some _ as q -> q
+      parse { opt with Query = q } rest
+    | [] -> opt
+
+let searchAndShowResult (client: FSharpApiSearchClient) (query: string) opt =
+  let results = client.Search(query, opt)
   results
   |> Seq.filter (fun x -> x.Distance < 3)
   |> Seq.iter (fun x ->
@@ -13,26 +45,66 @@ let searchAndShowResult (client: FSharpApiSearchClient) (query: string) =
     Console.WriteLine(sprintf ", distance: %d" x.Distance)
     Console.ResetColor()
   )
-  sw.Stop()
-  printfn "total %d [ms]" sw.ElapsedMilliseconds
+
+module Interactive =
+  type Lens<'a, 'b> = {
+    Get: 'a -> 'b
+    Set: 'b -> 'a -> 'a
+  }
+
+  let tryParseOptionStatus (x: string) =
+    match x.ToLower() with
+    | "enable" -> Some Enabled
+    | "disable" -> Some Disabled
+    | _ -> None
+
+  let (|OptionSetting|_|) (name: string) (lens: Lens<_, _>) opt (str: string) =
+    match str.Split([| ' ' |], 2) with
+    | [| key; value |] when key = name ->
+      match tryParseOptionStatus value with
+      | Some value -> Some (lens.Set value opt)
+      | None -> printfn "invalid value"; Some opt
+    | [| key |] when key = name ->
+      printfn "%A" (lens.Get opt)
+      Some opt
+    | _ -> None
+
+  let StrictQueryVariable = { Get = (fun x -> x.StrictQueryVariable ); Set = (fun value x -> { x with StrictQueryVariable = value }) }
+  let SimilaritySearching = { Get = (fun x -> x.SimilaritySearching ); Set = (fun value x -> { x with SimilaritySearching = value }) }
+
+  let rec loop (client: FSharpApiSearchClient) opt =
+    printf "> "
+    match System.Console.ReadLine() with
+    | "#q" -> opt
+    | OptionSetting "#strict" StrictQueryVariable opt newOpt -> loop client newOpt
+    | OptionSetting "#similarity" SimilaritySearching opt newOpt -> loop client newOpt
+    | query ->
+      try searchAndShowResult client query opt with ex -> printfn "%A" ex
+      loop client opt
+
+let helpMessage = """usage: FSharpApiSearch.exe <query> <options>
+
+query: Optional. If you omit will start in interactive mode.
+options:
+  --strict[+|-]
+      Enables or disables to strictly match the variables in the query.
+  --similarity[+|-]
+      Enables or disables the similarity searching.
+  --help
+      Print this message."""
 
 [<EntryPoint>]
 let main argv =
-  match argv with
-  | [| query |] ->
+  let args = Args.parse Args.empty (List.ofArray argv)
+  match args with
+  | { Help = true } ->
+    printfn "%s" helpMessage
+  | { Query = Some query } ->
     let client = FSharpApiSearchClient()
-    searchAndShowResult client query
-    0
-  | _ ->
-    printfn "initializing"
+    searchAndShowResult client query args.SearchOptions
+  | { Query = None } ->
+    printfn "Initializing."
     let client = FSharpApiSearchClient()
-    let rec loop () =
-      printfn "input query or #q to quit."
-      printf "> "
-      match System.Console.ReadLine() with
-      | "#q" -> ()
-      | query ->
-        try searchAndShowResult client query with ex -> printfn "%A" ex
-        loop()
-    loop()
-    0
+    printfn "Input query or #q to quit."
+    Interactive.loop client args.SearchOptions |> ignore
+  0
