10ac7b11fd6e1391818bf80acf457ebe2477eb8d
hafuu <hafuu46@gmail.com>
2016-04-27 07:01:18 +0900

    0.2.0-beta

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,2 +1,8 @@
+### 0.2.0-beta - 2016/4/27
+* Support type constraints at similarity searching
+* Exclude the modules (LanguagePrimitives and OperatorIntrinsics) for the F# compiler
+* Add ignore-argstyle option
+* Improve the start-up time by database
+
 ### 0.1.0-beta
 * Initial release
--- src/FSharpApiSearch/AssemblyInfo.fs
+++ src/FSharpApiSearch/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FSharpApiSearch")>]
 [<assembly: AssemblyProductAttribute("FSharpApiSearch")>]
 [<assembly: AssemblyDescriptionAttribute("F# API search engine")>]
-[<assembly: AssemblyVersionAttribute("0.1.0")>]
-[<assembly: AssemblyFileVersionAttribute("0.1.0")>]
+[<assembly: AssemblyVersionAttribute("0.2.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.2.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.1.0"
-    let [<Literal>] InformationalVersion = "0.1.0"
+    let [<Literal>] Version = "0.2.0"
+    let [<Literal>] InformationalVersion = "0.2.0"
