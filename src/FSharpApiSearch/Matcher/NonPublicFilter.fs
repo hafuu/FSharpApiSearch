a0861d4fd31d708fd52417fadfc2b9bd166566a9
hafuu <hafuu46@gmail.com>
2016-11-09 16:26:24 +0900

    Filter private type abbreviation.

--- src/FSharpApiSearch/Matcher/NonPublicFilter.fs
+++ src/FSharpApiSearch/Matcher/NonPublicFilter.fs
@@ -9,6 +9,7 @@ let testAccessibility ctx = function
 let test api ctx =
   match api.Signature with
   | ApiSignature.FullTypeDefinition { Accessibility = accessibility }
+  | ApiSignature.TypeAbbreviation { Accessibility = accessibility }
   | ApiSignature.ModuleDefinition { Accessibility = accessibility } -> testAccessibility ctx accessibility
   | _ -> Matched ctx
 
