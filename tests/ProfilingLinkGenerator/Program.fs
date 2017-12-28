b2f7c68644846f2fd376f80775ae771e5a491a5c
hafuu <hafuu46@gmail.com>
2017-12-28 11:23:27 +0900

    Tests for profiling of LinkGenerator.

---
+++ tests/ProfilingLinkGenerator/Program.fs
@@ -0,0 +1,17 @@
+﻿open FSharpApiSearch
+open System.Diagnostics
+
+[<EntryPoint>]
+let main argv = 
+  let database = Database.loadFromFile Database.databaseName
+  
+  let sw = Stopwatch.StartNew()
+
+  for i = 0 to 50 do
+    for apiDict in database do
+      for api in apiDict.Api do
+        LinkGenerator.dotNetApiBrowser "base" "view" api |> ignore
+
+  printfn "%d" sw.ElapsedMilliseconds
+
+  0
