5d9fba4b3170db76de8396c5f843f23cfe4673ab
hafuu <hafuu46@gmail.com>
2018-05-24 15:57:39 +0900

    Remove App.xaml

--- src/FSharpApiSearch.Desktop/Program.fs
+++ src/FSharpApiSearch.Desktop/Program.fs
@@ -5,5 +5,5 @@ open System.Windows
 
 [<STAThread; EntryPoint>]
 let main args =
-  let app = Application.LoadComponent(Uri("App.xaml", UriKind.Relative)) :?> Application
-  app.Run()
+  let window = Application.LoadComponent(Uri("MainWindow.xaml", UriKind.Relative)) :?> Window
+  Application().Run(window)
